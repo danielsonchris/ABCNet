5619f3baa1178a0d59b0068bfae3ffa714567ed5
Chris Danielson <danielsonchris@me.com>
2017-03-09 13:28:12 -0800

    Adding a build script.

---
+++ dev-bin/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`/..
+
+if [ -n "$DOTNETCORE" ]; then
+
+  echo Using .NET CLI
+
+  dotnet restore
+
+  # Running Benchmark
+  dotnet run -f netcoreapp1.0 -c $CONFIGURATION -p ./ExampleConsole
+
+  # Running Unit Tests
+  #dotnet test -f netcoreapp1.0 -c $CONFIGURATION ./ABCNet.Test
+
+else
+
+  echo Using Mono
+
+  cd mono
+
+  nuget restore
+
+  xbuild /p:Configuration=$CONFIGURATION
+
+  mono ../mono/packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --where "cat != BreaksMono" ./bin/$CONFIGURATION/ABCNet.Test.dll
+
+fi
